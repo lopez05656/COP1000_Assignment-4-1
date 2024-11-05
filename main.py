b357ac72731175e60d08e3042f44cd8f46af3c69
lopez05656 <lopzac981@gmail.com>
2024-11-04 20:10:43 -0500

    Initial Commit

--- main.py
+++ main.py
@@ -7,8 +7,27 @@ This program calculates prices for custom house signs.
 # Number of characters.
 # Color of characters.
 # Type of wood.
+charge = 35
+EXTRA_CHARGE = 4
+numChars = float(input("How many characters are in your customer's sign: "))
+color = str(input("Please enter your customer's preferred sign text color: "))
+woodType = str(input("Please Enter your customer's preferred wood type: "))
 
 # Write assignment and if statements here as appropriate.
+if numChars > 8:
+    charge += (numChars - 5) * EXTRA_CHARGE
+
+if woodType == str("Oak") or str("oak"):
+    charge = charge + 20
+
+#if woodType == str("Pine") or str("pine"):
+    #charge = charge - 20
+
+if color == str("Gold") or str("gold"):
+    charge += 15
+
+#if color == str("Black") or str("black"):
+    #charge = charge + 5
 
 # Output Charge for this sign.
-print("The charge for this sign is $" + str(charge))
+print("Your total for this order is $" + str(charge))
